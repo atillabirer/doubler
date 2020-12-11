e5d6dfe118a68a4db651b873f397705c11ea7a3a
Atilla Birer <bireratilla@gmail.com>
2020-12-11 12:47:55 +0300

    sqlite updates

--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,112 @@
-import BitcoinJsonRpc from 'bitcoin-json-rpc';
-import { NextFunction, Request, Response } from 'express';
+import BitcoinJsonRpc from "bitcoin-json-rpc";
+import { NextFunction, Request, Response } from "express";
 import express from "express";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 const app = express();
 import sqlite3 from "sqlite3";
 
 interface Investment {
-    address: string,
-    amount: number,
-    txid: string,
-    timestamp: number,
-    paid: boolean
+  address: string;
+  amount: number;
+  txid: string;
+  timestamp: number;
+  paid: boolean;
 }
 
-
 (async function () {
+  const rpc = new BitcoinJsonRpc("http://user:pass@localhost:19332");
 
-    const rpc = new BitcoinJsonRpc('http://user:pass@localhost:19332');
+  var investments: Investment[] = [];
 
-    var investments: Investment[] = [];
+  const db = new sqlite3.Database("database.db");
 
-    const db = new sqlite3.Database("database.db")
+  var depositaddress = await rpc.getNewAddress();
+  console.log(depositaddress);
 
-   var depositaddress = await rpc.getNewAddress()
-   console.log(depositaddress)
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.post(
+    "/",
+    async function (req: Request, res: Response, next: NextFunction) {
+      try {
+        //first check if we have balance to pay the previous person in the queue
+        //who was no tpaid
 
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(bodyParser.json())
-    app.post("/", async function (req: Request, res: Response, next: NextFunction) {
-        try {
-            //first check if we have balance to pay the previous person in the queue
-            //who was no tpaid
-            
-            if (req.body.txid) {
+        if (req.body.txid) {
+          let value = 0;
 
-                let value = 0;
-
-
-                const obj = await rpc.getRawTransactionAsObject(req.body.txid)
-                obj.vout.forEach((singlevout) => {
-                    let searchResult = singlevout.scriptPubKey.addresses?.find((address) => address === depositaddress)
-                    //this is the transaction output that paid to our deposit address
-                    //this is the value of what they sent to us
-                    if(searchResult) {
-                        value = singlevout.value!; //it exists damn you
-                    }
-                })
-                
-                console.log("receive tx:" + req.body.txid)
-                if (obj.vin) {
-                    let txid = obj.vin[0].txid;
-                    let vout = obj.vin[0].vout;
-                    console.log(txid);
-                    console.log(vout);
-                    if (txid) {
-                        let sendertx = await rpc.getRawTransactionAsObject(txid);
-
-                        let outputSearchResult = sendertx.vout.find((single) => single.n === vout)
-                        let sender = outputSearchResult?.scriptPubKey.addresses?.pop()
-                        console.log("Amount:",value)
-                        console.log("TXID: ",req.body.txid);
-                        console.log("Timestamp:",Date.now())
-                        if(!investments.find((investment) => investment.txid === req.body.txid)) {
-                            investments.push({address: sender!,amount: value,txid:req.body.txid,
-                                timestamp: Date.now(),paid: false})
-                                db.run("INSERT INTO investments(address,amount,txid,timestamp,paid) VALUES(?,?,?,?,?)",{address: sender!,amount: value,txid:req.body.txid,
-                                    timestamp: Date.now(),paid: false},(result: any,error: any) => {
-                                        console.log(result)
-                                        console.log(error)
-                                    })
-                        }
-                        console.log(investments)
-                        res.end()
-                        next()
-
-                    }
-                }
-            } else {
-                res.end()
-                next()
+          const obj = await rpc.getRawTransactionAsObject(req.body.txid);
+          obj.vout.forEach((singlevout) => {
+            let searchResult = singlevout.scriptPubKey.addresses?.find(
+              (address) => address === depositaddress
+            );
+            //this is the transaction output that paid to our deposit address
+            //this is the value of what they sent to us
+            if (searchResult) {
+              value = singlevout.value!; //it exists damn you
             }
-        } catch (error) {
-            next(error)
+          });
+
+          console.log("receive tx:" + req.body.txid);
+          if (obj.vin) {
+            let txid = obj.vin[0].txid;
+            let vout = obj.vin[0].vout;
+            console.log(txid);
+            console.log(vout);
+            if (txid) {
+              let sendertx = await rpc.getRawTransactionAsObject(txid);
+
+              let outputSearchResult = sendertx.vout.find(
+                (single) => single.n === vout
+              );
+              let sender = outputSearchResult?.scriptPubKey.addresses?.pop();
+              console.log("Amount:", value);
+              console.log("TXID: ", req.body.txid);
+              console.log("Timestamp:", Date.now());
+              if (
+                !investments.find(
+                  (investment) => investment.txid === req.body.txid
+                )
+              ) {
+                investments.push({
+                  address: sender!,
+                  amount: value,
+                  txid: req.body.txid,
+                  timestamp: Date.now(),
+                  paid: false,
+                });
+                db.run(
+                  "INSERT INTO investments(address,amount,txid,timestamp,paid) VALUES($address,$amount,$txid,$timestamp,$paid)",
+                  {
+                    $address: sender!,
+                    $amount: value,
+                    $txid: req.body.txid,
+                    $timestamp: Date.now(),
+                    $paid: false,
+                  },
+                  (result: any, error: any) => {
+                    console.log(result);
+                    console.log(error);
+                  }
+                );
+              }
+              console.log(investments);
+              res.end();
+              next();
+            }
+          }
+        } else {
+          res.end();
+          next();
         }
-    })
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
 
-    app.get("/transactions",function(req,res) {
-        res.json(investments)
-    })
-    app.listen(3000, () => console.log(":3000"))
-
-})()
-
-
-
-
-
-
-
+  app.get("/transactions", function (req, res) {
+    res.json(investments);
+  });
+  app.listen(3000, () => console.log(":3000"));
+})();
